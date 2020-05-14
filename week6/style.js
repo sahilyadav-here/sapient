ed3a92aec352bc15ed615664a32f968c69e38a49
Sahil Yadav <sahil16263@iiitd.ac.in>
2020-05-14 22:27:10 +0530

    added ntc functunality

--- week6/style.js
+++ week6/style.js
@@ -1,3 +1,5 @@
+// import { name } from './ntc.js';
+  
   window.addEventListener("load", function(){
     var text = document.getElementById("text");
     changeText(text);
@@ -12,5 +14,12 @@
     var font_name = style.fontFamily;
     var font_size = style.fontSize;
     var font_color = style.color;
-    text.innerHTML = ""+font_name + " " + font_size + " " + font_color;
+    font_color = font_color.split('(')[1].split(')')[0].split(',');
+    var r = parseInt(font_color[0].trim());
+    var g = parseInt(font_color[1].trim());
+    var b = parseInt(font_color[2].trim());
+    var colorHex = rgbToHex(r,g,b);
+    var colorName = ntc.name(colorHex);
+    
+    text.innerHTML = ""+font_name + " " + font_size + " " + colorName[1];
   }
